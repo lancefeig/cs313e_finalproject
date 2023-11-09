ff5d891eebab225db2d326b790de69bfd83d350a
lancefeig <feig@utexas.edu>
2023-11-08 18:46:28 -0600

    edit

--- warehouse.py
+++ warehouse.py
@@ -1,36 +1,67 @@
 import os
 
 HASH_TABLE_SIZE = 100
-DIRECTORY = os.path.dirname(os.path.abspath(__file__)) #saves pathway of this warehouse.py
+#saves pathway of this warehouse.py
+DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 
-class FileManager:
+class DataManager:
     def __init__(self):
-        self.data_path = os.path.join(DIRECTORY,"data.txt") #generates pathway of data.txt
+        #generates pathway of data.txt
+        self.data_path = os.path.join(DIRECTORY,"data.txt")
+        self.inventory = Inventory()
+        self.queue = OrderQueue()
 
     def data_in(self):
         try:
-            with open(self.data_path,"r") as data_file: #content manager
-                pass
-        except IOError: #catch error if data.txt does not exist
+            with open(self.data_path,"r",encoding="utf-8") as file:
+                n_chemicals = int(file.readline().strip)
+                n_orders = int(file.readline().strip)
+                properties_lst = file.readline().split()
+                #enter in n chemicals
+                for _ in range(n_chemicals):
+                    attributes = file.readline().split()
+                    # pop chemmical name and quantity
+                    name = attributes.pop(0)
+                    quantity = int(attributes.pop(0))
+                    properties = dict(zip(properties_lst, attributes))
+                    new = Chemical(name, quantity, properties)
+                    self.inventory.add_chemical(new)
+                #enter in n_orders (each order takes up 3 lines: customer & order id, chemicals, quantities)
+                for _ in range(n_orders*3):
+                    attributes = file.readline().split()
+                    customer = attributes[0]
+                    order_id = attributes[1]
+                    chemicals_lst = file.readline().split()
+                    quantities_lst = [int(x) for x in file.readline().split()]
+                    chemicals_dict = dict(zip(chemicals_lst, quantities_lst))
+                    self.queue.enqueue_order(order_id, customer, chemicals_dict)
+        #catch error if data.txt does not exist
+        except IOError:
             print("No data was found. Empty inventory initialized.")
 
     def data_out(self):
-        with open(self.data_path,"w") as data_file: #content manager
+        with open(self.data_path,"w",encoding="utf-8") as file:
             pass
+
+    def interface(self):
+        pass
 
 class Inventory:
     def __init__(self):
         self.hash_table = [[]]*HASH_TABLE_SIZE #empty 2-D array
+        self.n = 0
 
     def add_chemical(self, obj):
         if self.find_chemical(obj.name) is None:
             self.hash_table[obj.hash_key].append(obj)
+            self.n += 1
         print("Sorry, this chemical is already in the inventory. Please update the quantity instead.")
 
     def remove_chemical(self, name):
         obj = self.find_chemical(name)
         if obj is not None:
             self.hash_table[obj.hash_key].remove(obj)
+            self.n -= 1
         print("Sorry, this chemical is not in the inventory")
 
     def update_chemical(self, name, change):
@@ -49,38 +80,35 @@ class Inventory:
 
     def find_chemical(self, name):
         hash_key = name_to_key(name)
-
-        if self.hash_table[hash_key] == []: #check if bucket is empty
+        #check if bucket is empty
+        if self.hash_table[hash_key] == []:
             return None
-        for curr in self.hash_table[hash_key]: #linearly probe for chemical name existing in bucket
+        #linearly probe for chemical name existing in bucket
+        for curr in self.hash_table[hash_key]:
             if curr.name == name:
                 return curr
         return None
 
 class Chemical:
-    def __init__(self, name, properties, quantity=0):
+    def __init__(self, name, quantity, properties):
         self.name = name #string
-        self.properties = properties #dictionary of chemical properties
         self.quantity = quantity #integer
+        self.properties = properties #dictionary of chemical properties
         self.hash_key = name_to_key(name) #integer
 
 def name_to_key(string):
     #hash key based on the sum of the ASCII numbers
     return sum(list(map(lambda i: ord(i), string))) % HASH_TABLE_SIZE
 
-
-#wooooooo Ava's (probably) wrong and weird code!!
-#Also I've never done partner coding so If I'm infringing on some sorta
-#co-coding etiquette or code-y people rules I'm sorryyyyy
-class OrderTracking:
+class OrderQueue:
     def __init__(self):
         self.order_queue = [] #list of dictionaries
 
-    def enqueue_order(self, customer_details, chemical_list): #idk what should go in customer_details. Just name?
+    def enqueue_order(self, order_id, customer, chemicals_dict): #idk what should go in customer. Just name?
         new_order = {
-            'order_id': len(self.order_queue) + 1,
-            'customer_details': customer_details,
-            'chemical_list': chemical_list
+            'order_id': order_id,
+            'customer': customer,
+            'chemicals_dict': chemicals_dict
         }
         self.order_queue.append(new_order)
         self.order_queue.sort(key=lambda x: x['order_id'])  #make sure the records are sorted by order ID for binary search
@@ -88,7 +116,7 @@ class OrderTracking:
     def process_order_queue(self):#What are we supposed to DO with the processed order?
         if len(self.order_queue) > 0: #check if empty
             processed_order = self.order_queue.pop(0)#removes first one as processed
-            print(f"Processing order {processed_order['order_id']} for {processed_order['customer_details']}")
+            print(f"Processing order {processed_order['order_id']} for {processed_order['customer']}")
         else:
             print("No orders to process.")
 
@@ -107,3 +135,8 @@ class OrderTracking:
                 high = mid - 1
 
         return None
+
+def main():
+    pass
+
+main()
